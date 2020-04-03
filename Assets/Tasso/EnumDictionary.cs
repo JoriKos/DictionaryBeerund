79a169e1ecc970573d60c5d8bcfd5ab5b7612239
26689 <tasso.mevissen@gmail.com>
2020-04-03 11:22:56 +0200

    finished EnumDictionary

--- Assets/Tasso/EnumDictionary.cs
+++ Assets/Tasso/EnumDictionary.cs
@@ -6,15 +6,30 @@ using UnityEngine.UI;
 
 public class EnumDictionary : MonoBehaviour
 {
-  
-    private Dictionary<Weapons, int> weapons = new Dictionary<Weapons, int>();
 
-    enum Weapons { M4, M16, Shotgun, Sniper };
+    Dictionary<Weapons, int> weaponAmmo = new Dictionary<Weapons, int>();
 
-    private void Awake()
+    enum Weapons
     {
-
-
+        RocketLauncher,
+        PPSh,
+        LeeEnfield
     }
-    
+    void Start()
+    {
+        weaponAmmo.Add(Weapons.RocketLauncher, 4);
+        weaponAmmo.Add(Weapons.PPSh, 71);
+        weaponAmmo.Add(Weapons.LeeEnfield, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.space))
+        {
+            weaponAmmo[Weapons.RocketLauncher] -= 1;
+            Debug.Log(weaponAmmo[Weapons.RocketLauncher]);
+        }
+    }
+
 }
